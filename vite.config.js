bc4a570b888e3ced53fb766129bf2e441da5a587
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2023-10-26 18:31:40 +0700

    Vite Config JS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,101 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+import fs from 'fs-extra';
+import path from 'path';
+
+const folder = {
+    src: "resources/", // source files
+    src_assets: "resources/", // source assets files
+    dist: "public/", // build files
+    dist_assets: "public/build/" //build assets files
+};
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        rtl: true,
+        outDir: 'public/build/',
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (css) => {
+                    if (css.name.split('.').pop() == 'css') {
+                        return 'css/' + `[name]` + '.min.' + 'css';
+                    } else {
+                        return 'icons/' + css.name;
+                    }
+                },
+                entryFileNames: 'js/' + `[name]` + `.js`,
+            },
+        },
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+        laravel(
+            {
+                input: [
+                    'resources/scss/bootstrap.scss',
+                    'resources/scss/icons.scss',
+                    'resources/scss/app.scss',
+                    'resources/scss/custom.scss',
+                ],
+                refresh: true
+            }
+        ),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'resources/fonts',
+                    dest: ''
+                },
+                {
+                    src: 'resources/images',
+                    dest: ''
+                },
+                {
+                    src: 'resources/js',
+                    dest: ''
+                },
+                {
+                    src: 'resources/json',
+                    dest: ''
+                },
+                {
+                    src: 'resources/libs',
+                    dest: ''
+                },
+            ]
         }),
+        {
+            name: 'copy-specific-packages',
+            async writeBundle() {
+                const outputPath = path.resolve(__dirname, folder.dist_assets); // Adjust the destination path
+                const configPath = path.resolve(__dirname, 'package-copy-config.json');
+
+                try {
+                    const configContent = await fs.readFile(configPath, 'utf-8');
+                    const { packagesToCopy } = JSON.parse(configContent);
+
+                    for (const packageName of packagesToCopy) {
+                        const destPackagePath = path.join(outputPath, 'libs', packageName);
+
+                        const sourcePath = (fs.existsSync(path.join(__dirname, 'node_modules', packageName + "/dist"))) ?
+                            path.join(__dirname, 'node_modules', packageName + "/dist")
+                            : path.join(__dirname, 'node_modules', packageName);
+
+                        try {
+                            await fs.access(sourcePath, fs.constants.F_OK);
+                            await fs.copy(sourcePath, destPackagePath);
+                        } catch (error) {
+                            console.error(`Package ${packageName} does not exist.`);
+                        }
+                    }
+                } catch (error) {
+                    console.error('Error copying and renaming packages:', error);
+                }
+            },
+        },
+
     ],
 });
