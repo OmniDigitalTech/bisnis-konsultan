ccb1fd90e6f94934602647c371b1187c49ff9b42
Ancasea <rekasae255@gmail.com>
2023-10-28 23:28:43 +0700

    add set favicon&meta tag

--- Modules/Website/Resources/views/layouts/master.blade.php
+++ Modules/Website/Resources/views/layouts/master.blade.php
@@ -2,13 +2,45 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-topbar="light">
 
 <head>
-    <meta charset="utf-8" />
-    <title>@yield('title')</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <title>@yield('title')</title>
+    <meta name="title" content="Snapbiz Consulting">
+    <meta name="description" content="
+        Dengan pengalaman luas dalam analisis data, strategi pemasaran, dan manajemen operasional.
+        Kami adalah mitra terpercaya untuk membantu pertumbuhan bisnis Anda.">
+    <meta name="keywords" content="
+        bisnis, konsultan, konsultasi, akuntansi, audit, laporan keuangan, business, consultant,
+         consulting, accounting, audit, financial reports">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow" />
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="1 days">
+    <meta name="author" content="Omni Digital Technology">
+
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ URL::asset("build/images/favicons/apple-touch-icon-57x57.png") }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ URL::asset("build/images/favicons/apple-touch-icon-60x60.png") }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ URL::asset("build/images/favicons/apple-touch-icon-72x72.png") }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset("build/images/favicons/apple-touch-icon-76x76.png") }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ URL::asset("build/images/favicons/apple-touch-icon-114x114.png") }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset("build/images/favicons/apple-touch-icon-120x120.png") }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ URL::asset("build/images/favicons/apple-touch-icon-144x144.png") }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset("build/images/favicons/apple-touch-icon-152x152.png") }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::asset("build/images/favicons/apple-touch-icon-180x180.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::asset("build/images/favicons/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ URL::asset("build/images/favicons/android-chrome-192x192.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::asset("build/images/favicons/favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ URL::asset("build/images/favicons/site.webmanifest") }}">
+    <link rel="mask-icon" href="{{ URL::asset("build/images/favicons/safari-pinned-tab.svg") }}" color="#5bbad5">
+    <link rel="shortcut icon" href="{{ URL::asset("build/images/favicons/favicon.ico") }}">
+    <meta name="apple-mobile-web-app-title" content="Snapbiz Consulting">
+    <meta name="application-name" content="Snapbiz Consulting">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="msapplication-TileImage" content="{{ URL::asset("build/images/favicons/mstile-144x144.png") }}">
+    <meta name="msapplication-config" content="{{ URL::asset("build/images/favicons/browserconfig.xml") }}">
+    <meta name="theme-color" content="#ffffff">
+
     @include('website::layouts.head-css')
 </head>
 
---
+++ resources/images/favicons/README.md
@@ -0,0 +1,35 @@
+# Your Favicon Package
+
+This package was generated with [RealFaviconGenerator](https://realfavicongenerator.net/) [v0.16](https://realfavicongenerator.net/change_log#v0.16)
+
+## Install instructions
+
+To install this package:
+
+Extract this package in <code>&lt;web site&gt;/build/images/favicons/</code>. If your site is <code>http://www.example.com</code>, you should be able to access a file named <code>http://www.example.com/build/images/favicons/favicon.ico</code>.
+
+Insert the following code in the `head` section of your pages:
+
+    <link rel="apple-touch-icon" sizes="57x57" href="build/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="build/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="build/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="build/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="build/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="build/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="build/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="build/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="build/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="build/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="build/images/favicons/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="build/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="build/images/favicons/site.webmanifest">
+    <link rel="mask-icon" href="build/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="build/images/favicons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Snapbiz Consulting">
+    <meta name="application-name" content="Snapbiz Consulting">
+    <meta name="msapplication-TileColor" content="#ffc40d">
+    <meta name="msapplication-TileImage" content="build/images/favicons/mstile-144x144.png">
+    <meta name="msapplication-config" content="build/images/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+*Optional* - Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
